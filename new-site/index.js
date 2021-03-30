8de116371994b5aa330c32415311c9b77dcb6abc
Bulintiș Ștefan-Alexandru <alexbulintis@icloud.com>
2021-03-30 23:17:33 +0900

    new site layout

--- new-site/index.js
+++ new-site/index.js
@@ -2,10 +2,24 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "pug");
+app.use(express.static('public'));
 
 app.get("/", async (req, res) => {
   res.render("index");
 });
+
+app.get("/login", async (req, res) => {
+  res.render("login");
+});
+
+
+/* Student views */
+
+app.get("/student", async (req, res) => {
+  res.render("student/index");
+});
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
